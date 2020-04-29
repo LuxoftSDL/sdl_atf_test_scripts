462e9ad39598ab101a6c84f58a54e6b8b3f4f47a
Galina <hsavynetska@luxoft.com>
2020-04-29 13:04:26 +0300

    Initial scripts for New vehicle data WindowStatus

---
+++ test_scripts/API/VehicleData/WindowStatus/007_SubscribeVD_success_flow_with_windowStatus_param.lua
@@ -0,0 +1,31 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal:https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0261-New-vehicle-data-WindowStatus.md
+--
+-- Description: Successful processing of SubscribeVehicleData with `windowStatus` parameter.
+--
+-- In case:
+-- 1) App sends SubscribeVehicleData request with windowStatus=true to the SDL and this request is allowed by Policies.
+-- SDL does:
+--  a) transfer this request to HMI.
+-- 2) HMI responds with `SUCCESS` result to SubscribeVehicleData request and with not success result to `windowStatus` vehicle data
+-- SDL does:
+--  a) respond `SUCCESS`, success:true and with `windowStatus` data to mobile application.
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/API/VehicleData/WindowStatus/common')
+
+--[[ Local Variables ]]
+local rpc = "SubscribeVehicleData"
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("Register App", common.registerApp)
+common.Step("Activate App", common.activateApp)
+
+common.Title("Test")
+common.Step("App subscribes to windowStatus data", common.subUnScribeVD, { rpc })
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
