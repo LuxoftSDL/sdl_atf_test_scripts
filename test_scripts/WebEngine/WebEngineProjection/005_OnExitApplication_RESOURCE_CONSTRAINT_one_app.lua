cdb410a6d50cc3475f734832226d62b8758b8773
Oleksandr Deriabin (GitHub) <aderiabin@luxoft.com>
2020-07-01 18:33:10 +0300

    Add test scripts for OnExitApplication notification with reason RESOURCE_CONSTRAINT

---
+++ test_scripts/WebEngine/WebEngineProjection/005_OnExitApplication_RESOURCE_CONSTRAINT_one_app.lua
@@ -0,0 +1,34 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0273-webengine-projection-mode.md
+--
+-- Description:
+-- Processing of the OnExitApplication notification with reason RESOURCE_CONSTRAINT from HMI
+--  (single application)
+--
+-- Precondition:
+-- 1. SDL and HMI are started
+-- 2. App is registered on default mobile connection
+-- 4. App is activated
+--
+-- Steps:
+-- 1. HMI sends BC.OnExitApplication with reason: "RESOURCE_CONSTRAINT" related to App to SDL
+-- SDL does:
+--  - unregister App and send OnAppInterfaceUnregistered notification with reason: "RESOURCE_CONSTRAINT" to it
+--  - send BasicCommunication.OnAppUnregistered notification related to App with unexpectedDisconnect: false
+--  - close mobile connection
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/WebEngine/commonWebEngine')
+
+-- [[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI", common.start)
+common.Step("Register App", common.registerAppWOPTU)
+common.Step("Activate App", common.activateApp)
+
+common.Title("Test")
+common.Step("OnExitApplication", common.processResourceConstraintExit)
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
