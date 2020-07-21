fd8b6189ea63d0014aae1f57f4045fad6a4fc1ff
IGetmanets <igetmanets@luxoft.com>
2020-07-21 14:20:35 +0300

    fixup! Add scripts related to the OnAppCapabilityUpdated

--- test_scripts/UpdateVideoStreamingCapabilities/OnAppCapabilityUpdated/010_OnAppCapabilityUpdated_VCS_is_mismatched_with_additional_VCS_received_from_HMI_on_start.lua
+++ test_scripts/UpdateVideoStreamingCapabilities/OnAppCapabilityUpdated/010_OnAppCapabilityUpdated_VCS_is_mismatched_with_additional_VCS_received_from_HMI_on_start.lua
@@ -2,24 +2,27 @@
 -- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0296-Update-video-streaming-capabilities-during-ignition-cycle.md
 --
 -- Description:
--- Processing OnAppCapabilityUpdated notification from mobile to HMI
+-- Processing OnAppCapabilityUpdated notification with videoStreamingCapability that doesn't match
+--  with the videoStreamingCapability received from HMI on start from mobile to HMI
 --
 -- Precondition:
 -- 1. SDL and HMI are started
--- 2. App1 with NAVIGATION appHMIType is registered
--- 3. OnAppCapabilityUpdated notification is alowed by policy for App1
+-- 2. App with `NAVIGATION` appHMIType and 5 transport protocol is registered
+-- 3. OnAppCapabilityUpdated notification is allowed by policy for App
 --
 -- Sequence:
--- 1. App1 sends OnAppCapabilityUpdated without mandatory parameter appCapabilityType
--- 2. SDL doesn't send OnAppCapabilityUpdated notification to the HMI
-
+-- 1. App sends OnAppCapabilityUpdated for VIDEO_STREAMING capability type with videoStreamingCapability that doesn't
+--    match with the videoStreamingCapability received from HMI on start
+-- SDL does:
+-- - a. send OnAppCapabilityUpdated notification to the HMI
 ---------------------------------------------------------------------------------------------------
 -- [[ Required Shared libraries ]]
 local common = require('test_scripts/UpdateVideoStreamingCapabilities/common')
 
+--[[ Local Variables ]]
 local appCapability = {
   appCapability = {
-    -- appCapabilityType - mandatory parameter is missed
+    appCapabilityType = "VIDEO_STREAMING",
     videoStreamingCapability = common.getVideoStreamingCapability()
   }
 }
@@ -27,15 +30,13 @@ local appCapability = {
 --[[ Scenario ]]
 common.Title("Preconditions")
 common.Step("Clean environment", common.preconditions)
-common.Step("Prepare preloaded policy table", common.preparePreloadedPT)
-common.Step("Set HMI Capabilities", common.setHMICapabilities)
-common.Step("Start SDL, HMI, connect Mobile, start Session", common.start, { common.hmiDefaultCapabilities })
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
 common.Step("RAI", common.registerAppWOPTU)
 common.Step("Activate App", common.activateApp)
 
 common.Title("Test")
-common.Step("App sends OnAppCapabilityUpdated without mandatory parameter appCapabilityType", 
-	common.sendOnAppCapabilityUpdated, { appCapability, 0 } )
+common.Step("App sends OnAppCapabilityUpdated with new videoStreamingCapability", common.sendOnAppCapabilityUpdated,
+	{ appCapability } )
 
 common.Title("Postconditions")
 common.Step("Stop SDL", common.postconditions)
