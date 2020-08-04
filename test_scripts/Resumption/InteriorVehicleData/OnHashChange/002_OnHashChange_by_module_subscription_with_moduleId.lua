123c20197e3a8ba6dfd6d6127e8854f2fad74199
IGetmanets <igetmanets@luxoft.com>
2020-08-04 17:06:15 +0300

    Tests for OnHashChange

---
+++ test_scripts/Resumption/InteriorVehicleData/OnHashChange/002_OnHashChange_by_module_subscription_with_moduleId.lua
@@ -0,0 +1,42 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0188-get-interior-data-resumption.md
+--
+-- Description: SDL sends OnHashChange notification after successful subscription to interior vehicle data
+--  with custom moduleId
+--
+-- Precondition:
+-- 1. HMI and SDL are started
+-- 2. Mobile app with REMOTE_CONTROL hmi type is registered and activated
+-- 3. App is not subscribed to modules
+--
+-- Sequence:
+-- 1. GetInteriorVD(subscribe = true, module_1, moduleId) is requested
+-- SDL does:
+-- - a. send GetInteriorVD(subscribe = true, module_1, moduleId) request to HMI
+-- - b. process successful responses from HMI
+-- - c. send OnHashChange notification to mobile app by receiving GetInteriorVD(subscribe = true, module_1, moduleId)
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/Resumption/InteriorVehicleData/commonResumptionsInteriorVD')
+
+--[[ Local Variables ]]
+local moduleIdNumber = 2
+local expectNotif = 1
+local isSubscribed = true
+local isNotCached = false
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("App registration", common.registerAppWOPTU)
+common.Step("App activation", common.activateApp)
+
+common.Title("Test")
+for _, moduleName in pairs(common.modules)do
+  common.Step("OnHashChange after adding subscription to " .. moduleName, common.GetInteriorVehicleData,
+    { moduleName, moduleIdNumber, isSubscribed, isNotCached, expectNotif })
+end
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
---
+++ test_scripts/Resumption/InteriorVehicleData/OnHashChange/004_OnHashChange_by_module_unsubscription_with_moduleId.lua
@@ -0,0 +1,45 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0188-get-interior-data-resumption.md
+--
+-- Description: SDL sends OnHashChange notification after successful unsubscription from interior vehicle data
+--  with custom moduleId
+--
+-- Precondition:
+-- 1. HMI and SDL are started
+-- 2. Mobile app with REMOTE_CONTROL hmi type is registered and activated
+-- 3. App is subscribed to modules
+--
+-- Sequence:
+-- 1. GetInteriorVD(subscribe = false, module_1, moduleId) is requested
+-- SDL does:
+-- - a. send GetInteriorVD(subscribe = false, module_1, moduleId) request to HMI
+-- - b. process successful responses from HMI
+-- - c. send OnHashChange notification to mobile app by receiving GetInteriorVD(subscribe = false, module_1, moduleId)
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/Resumption/InteriorVehicleData/commonResumptionsInteriorVD')
+
+--[[ Local Variables ]]
+local moduleIdNumber = 2
+local expectNotif = 1
+local isSubscribed = true
+local isNotSubscribed = false
+local isNotCached = false
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("App registration", common.registerAppWOPTU)
+common.Step("App activation", common.activateApp)
+
+common.Title("Test")
+for _, moduleName in pairs(common.modules)do
+  common.Step("Subscription to " .. moduleName, common.GetInteriorVehicleData,
+    { moduleName, moduleIdNumber, isSubscribed, isNotCached, expectNotif })
+  common.Step("OnHashChange after removing subscription for " .. moduleName, common.GetInteriorVehicleData,
+    { moduleName, moduleIdNumber, isNotSubscribed, isNotCached, expectNotif })
+end
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
