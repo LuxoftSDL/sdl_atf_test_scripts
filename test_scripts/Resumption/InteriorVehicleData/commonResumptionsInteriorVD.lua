142b8b7f3b366e6b1dc27cff160a82adc7199a27
IGetmanets <igetmanets@luxoft.com>
2020-08-06 10:19:22 +0300

    Update for common file

--- test_scripts/Resumption/InteriorVehicleData/commonResumptionsInteriorVD.lua
+++ test_scripts/Resumption/InteriorVehicleData/commonResumptionsInteriorVD.lua
@@ -7,6 +7,8 @@ local utils = require("user_modules/utils")
 local json = require("modules/json")
 local runner = require('user_modules/script_runner')
 local rc = require('user_modules/sequences/remote_control')
+local SDL = require('SDL')
+local color = require("user_modules/consts").color
 
 --[[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
@@ -27,7 +29,6 @@ m.hashId = {}
 m.Title = runner.Title
 m.Step = runner.Step
 m.postconditions = actions.postconditions
-m.preconditions = rc.preconditions
 m.start = rc.rc.start
 m.registerAppWOPTU = actions.registerAppWOPTU
 m.activateApp = actions.activateApp
@@ -38,6 +39,16 @@ m.postconditions = actions.postconditions
 m.getHMIConnection = actions.hmi.getConnection
 m.getMobileSession = actions.mobile.getSession
 m.getHMIAppId = actions.app.getHMIId
+m.mobileDisconnect = actions.mobile.disconnect
+m.mobileConnect = actions.mobile.connect
+m.getConfigAppParams =  actions.getConfigAppParams
+m.setHMIAppId = actions.app.setHMIId
+m.isTableEqual = utils.isTableEqual
+m.fail = actions.run.fail
+
+--[[ General configuration ]]
+local state = rc.state.buildDefaultActualModuleState(rc.predefined.getRcCapabilities())
+rc.state.initActualModuleStateOnHMI(state)
 
 -- [[ Common Functions ]]
 local function boolToTimes(isTrue)
@@ -47,15 +58,14 @@ local function boolToTimes(isTrue)
   return 0
 end
 
-function  m.GetInteriorVehicleData(pModuleType, pModuleIdNumber, isSubscribe, pIsIVDataCached, hashChangeExpectTimes, pAppId)
+function  m.GetInteriorVehicleData(pModuleType, pModuleId, isSubscribe, pIsIVDataCached, hashChangeExpectTimes, pAppId)
   pAppId = pAppId or 1
   local rpc = "GetInteriorVehicleData"
+  if pIsIVDataCached == nil then pIsIVDataCached = false end
 
-  local moduleId = m.getModuleId(pModuleType, pModuleIdNumber or 1)
+  local moduleId = pModuleId or m.getModuleId(pModuleType,  1)
   local mobileRequestParams = m.cloneTable(rc.rpc.getAppRequestParams(rpc, pModuleType, moduleId, isSubscribe))
-  if pModuleIdNumber == nil then
-    mobileRequestParams.moduleId = nil
-  end
+
   local hmiRequestParams = rc.rpc.getHMIRequestParams(rpc, pModuleType, moduleId, pAppId, isSubscribe)
   if hashChangeExpectTimes == 0 then hmiRequestParams.subscribe = nil end
 
@@ -73,16 +83,17 @@ function  m.GetInteriorVehicleData(pModuleType, pModuleIdNumber, isSubscribe, pI
   :Do(function(_,data)
       m.hashId[pAppId] = data.payload.hashID
     end)
-  :Times(hashChangeExpectTimes)
+  :Times(hashChangeExpectTimes or 1)
 end
 
 function m.getModuleId(pModuleType, pModuleIdNumber)
-  local out = m.cloneTable(rc.predefined.getModuleControlData(pModuleType, pModuleIdNumber))
+  local out = m.cloneTable(m.getModuleControlData(pModuleType, pModuleIdNumber))
   return out.moduleId
 end
 
 function m.onInteriorVD(pModuleType, pModuleId, pHasSubscription, pAppId)
-  rc.isSubscribed(pModuleType, pModuleId or m.getModuleId(pModuleType, 1), pAppId, true or pHasSubscription)
+  if pHasSubscription == nil then pHasSubscription = true end
+  rc.rc.isSubscribed(pModuleType, pModuleId or m.getModuleId(pModuleType, 1), pAppId, pHasSubscription)
 end
 
 local function getRCAppConfig(pPt)
@@ -126,6 +137,101 @@ function m.preconditions(isPreloadedUpdate, pCountOfRCApps)
   if isPreloadedUpdate == true then
     updatePreloadedPT(pCountOfRCApps)
   end
+end
+
+function m.reRegisterApp(pAppId, pCheckResumptionData, pCheckResumptionHMILevel, pResultCode)
+  if not pAppId then pAppId = 1 end
+  if not pResultCode then pResultCode = "SUCCESS" end
+  local mobSession = actions.mobile.createSession(pAppId)
+  mobSession:StartService(7)
+  :Do(function()
+      local params = m.cloneTable(m.getConfigAppParams(pAppId))
+      params.hashID = m.hashId[pAppId]
+      local corId = mobSession:SendRPC("RegisterAppInterface", params)
+      m.getHMIConnection():ExpectNotification("BasicCommunication.OnAppRegistered", {
+          application = { appName = m.getConfigAppParams(pAppId).appName }
+        })
+      :Do(function(_, data)
+          m.setHMIAppId(data.params.application.appID, pAppId)
+        end)
+      mobSession:ExpectResponse(corId, { success = true, resultCode = pResultCode })
+    end)
+  pCheckResumptionData()
+  pCheckResumptionHMILevel(pAppId)
+end
+
+function m.resumptionFullHMILevel(pAppId)
+  m.getHMIConnection():ExpectRequest("BasicCommunication.ActivateApp", { appID = m.getHMIAppId(pAppId) })
+  :Do(function(_, data)
+      m.getHMIConnection():SendResponse(data.id, "BasicCommunication.ActivateApp", "SUCCESS", {})
+    end)
+  m.getMobileSession(pAppId):ExpectNotification("OnHMIStatus",
+    { hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN" },
+    { hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE" })
+  :Times(2)
+end
+
+function m.checkModuleResumptionData(pModuleType, pModuleId)
+  local requestParams = {
+    moduleType = pModuleType,
+    subscribe = true,
+    moduleId = pModuleId
+  }
+  EXPECT_HMICALL("RC.GetInteriorVehicleData", requestParams)
+  :Do(function(_, data)
+      m.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS",
+        { moduleData = m.state.getActualModuleIVData(pModuleType, pModuleId)})
+    end)
+end
+
+function m.getModuleIdNumber(pModuleType, pModuleId)
+  local moduleNumbers = 2
+  for i = 1, moduleNumbers do
+    local out = m.getModuleControlData(pModuleType, i)
+    if out.moduleId == pModuleId then
+      return i
+    end
+  end
+  m.fail(pModuleId .. " is not found for " .. pModuleType .. " in predefinedInteriorVehicleData")
+end
+
+function m.getModuleControlData(pModuleType, pModuleId)
+  local out = rc.predefined.getModuleControlData(pModuleType, 1)
+  if pModuleId == 1 or nil then
+    return out
+  else
+    local actualData = rc.state.getActualModuleStateOnHMI()
+    for key, value in pairs(actualData[pModuleType]) do
+      if key ~= out.moduleId then
+        return value.data
+      end
+    end
+  end
+    utils.cprint(color.magenta, "There is only one moduleId for module " .. pModuleType ..
+      ".\nChecked default moduleId." )
+  return out
+end
+
+function m.ignitionOff()
+  local isOnSDLCloseSent = false
+  m.getHMIConnection():SendNotification("BasicCommunication.OnExitAllApplications", { reason = "SUSPEND" })
+  m.getHMIConnection():ExpectNotification("BasicCommunication.OnSDLPersistenceComplete")
+  :Do(function()
+    m.getHMIConnection():SendNotification("BasicCommunication.OnExitAllApplications", { reason = "IGNITION_OFF" })
+    m.getHMIConnection():ExpectNotification("BasicCommunication.OnSDLClose")
+    :Do(function()
+      isOnSDLCloseSent = true
+      SDL.DeleteFile()
+    end)
+  end)
+  m.wait(3000)
+  :Do(function()
+    if isOnSDLCloseSent == false then m.cprint(color.magenta, "BC.OnSDLClose was not sent") end
+    for i = 1, actions.mobile.getAppsCount() do
+      actions.mobile.deleteSession(i)
+    end
+    StopSDL()
+  end)
 end
 
 return m
