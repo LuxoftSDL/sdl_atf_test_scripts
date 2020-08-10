0c8a9941681f865ce3ffcb0e2460758f0943dffb
vjacheslav <vklepikov@luxoft.com>
2020-08-10 16:16:59 +0300

    Scripts for double subscriptions

---
+++ test_scripts/Resumption/InteriorVehicleData/026_InteriorVD_resumption_double_subscription_with_moduleId_unexpected_disconnect.lua
@@ -0,0 +1,93 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0188-get-interior-data-resumption.md
+--
+-- Description: SDL sends GetInteriorVehicleData response with WARNINGS result code
+--  in case of double subscription with specified moduleId after successful resuming on transport disconnect
+--
+-- Precondition:
+-- 1. HMI and SDL are started
+-- 2. Mobile app with REMOTE_CONTROL hmi type is registered and activated
+-- 3. App is subscribed to all modules via GetInteriorVehicleData(moduleType, moduleId)
+-- 4. Unexpected disconnect and reconnect are performed
+-- 5. App reregisters with actual HashId after unexpected disconnect
+-- 6. HMI responds with SUCCESS resultCode to all requests from SDL
+--
+-- Sequence:
+-- 1. GetInteriorVehicleData(subscribe = true, module_1, moduleId) is requested
+-- SDL does:
+-- - a. not send GetInteriorVehicleData request to HMI
+-- - b. send GetInteriorVehicleData ("WARNINGS") response to mobile App
+-- - c. not send OnHashChange notification to mobile app
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/Resumption/InteriorVehicleData/commonResumptionsInteriorVD')
+
+--[[ Local Variables ]]
+local moduleIdNumber = 2
+local notExpectNotif = 0
+local expectNotif = 1
+local isSubscribed = true
+local isCached = true
+local isNotCached = false
+local appSessionId = 1
+local resultCode = "WARNINGS"
+
+
+--[[ Local Functions ]]
+local function checkResumptionData()
+  local actualModules = { }
+  local expectedModules = { }
+
+  for key, moduleType in pairs(common.modules) do
+    expectedModules[key] = {
+      moduleType = moduleType,
+      moduleId = common.getModuleId(moduleType, moduleIdNumber)
+    }
+  end
+
+  common.getHMIConnection():ExpectRequest("RC.GetInteriorVehicleData")
+  :Do(function(_, data)
+      common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", {
+        moduleData = common.getActualModuleIVData(data.params.moduleType, data.params.moduleId), isSubscribed = true })
+    end)
+  :ValidIf(function(exp, data)
+    actualModules[exp.occurences] = {
+      moduleType = data.params.moduleType,
+      moduleId = data.params.moduleId
+    }
+    if exp.occurences == #common.modules then
+      if common.isTableEqual(actualModules, expectedModules) == false then
+        local errorMessage = "Not all modules are resumed.\n" ..
+          "Actual result:" .. common.tableToString(actualModules) .. "\n" ..
+          "Expected result:" .. common.tableToString(expectedModules) .."\n"
+        return false, errorMessage
+      end
+    end
+    return true
+  end)
+  :Times(#common.modules)
+end
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("App registration", common.registerAppWOPTU)
+common.Step("App activation", common.activateApp)
+for _, moduleType in pairs(common.modules)do
+  common.Step("Subscription to " .. moduleType, common.GetInteriorVehicleData,
+    { moduleType, common.getModuleId(moduleType, moduleIdNumber), isSubscribed, isNotCached, expectNotif })
+end
+common.Step("Unexpected disconnect", common.mobileDisconnect)
+common.Step("Connect mobile", common.mobileConnect)
+common.Step("Re-register App resumption data", common.reRegisterApp,
+  { appSessionId, checkResumptionData, common.resumptionFullHMILevel })
+
+common.Title("Test")
+for _, moduleType in pairs(common.modules)do
+  common.Step("Second subscription to " .. moduleType , common.GetInteriorVehicleData,
+    { moduleType, moduleIdNumber, isSubscribed, isCached, notExpectNotif, appSessionId, resultCode })
+end
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
