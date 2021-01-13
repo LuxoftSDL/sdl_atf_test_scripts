4f55b3dd38ca4f695245dd98ea208d0eed70d58c
vjacheslav <vklepikov@luxoft.com>
2021-01-13 12:31:23 +0200

    fix for script 018_2 multiple apps

--- test_scripts/Protocol/Enable_OEM_exclusive_apps_support/018_2_GetSI_and_GetVT_delayed_multiple_Apps.lua
+++ test_scripts/Protocol/Enable_OEM_exclusive_apps_support/018_2_GetSI_and_GetVT_delayed_multiple_Apps.lua
@@ -7,15 +7,42 @@ local common = require("test_scripts/Protocol/commonProtocol")
 --[[ Local Variables ]]
 local delay1 = 2000
 local delay2 = 3000
-local exp_delay = delay1 + delay2
-local tolerance = 500
-
 local hmiCap = common.setHMIcap(common.vehicleTypeInfoParams.default)
-  hmiCap.BasicCommunication.GetSystemInfo.delay = delay1
-  hmiCap.VehicleInfo.GetVehicleType.delay = delay2
 
 --[[ Local Functions ]]
-local function startServiceMultipleApps(pStartEvent)
+local function delayedStartServiceAckMultipleApps(pHmiCap, pDelayGetSI, pDelayGetVT)
+  local rpcServiceAckParams = common.getRpcServiceAckParams(pHmiCap)
+  local getSIparams = pHmiCap.BasicCommunication.GetSystemInfo.params
+  local getVTparams = pHmiCap.VehicleInfo.GetVehicleType.params
+  pHmiCap.VehicleInfo.GetVehicleType = nil
+  pHmiCap.BasicCommunication.GetSystemInfo = nil
+
+  local ts_get_si
+  local ts_get_vt
+
+  common.init.HMI_onReady(pHmiCap)
+
+  common.hmi.getConnection():ExpectRequest("BasicCommunication.GetSystemInfo")
+  :Do(function(_, data)
+      local function response()
+        ts_get_si = timestamp()
+        common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", getSIparams)
+      end
+      common.run.runAfter(response, pDelayGetSI)
+    end)
+
+  local times_GetVT = 1
+  if pDelayGetVT == -1 then times_GetVT = 0 end
+  common.hmi.getConnection():ExpectRequest("VehicleInfo.GetVehicleType")
+  :Do(function(_, data)
+      local function response()
+        ts_get_vt = timestamp()
+        common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", getVTparams)
+      end
+      if pDelayGetVT ~= -1 then common.run.runAfter(response, pDelayGetVT) end
+    end)
+   :Times(times_GetVT)
+
   local reqParams = { protocolVersion = common.setStringBsonValue("5.3.0") }
   local mobSession = common.getMobileSession()
   local msg = {
@@ -35,32 +62,41 @@ local function startServiceMultipleApps(pStartEvent)
   local ts_req2 = timestamp()
   common.log("MOB->SDL: App2" .." StartService(7) " .. common.tableToString(reqParams))
 
-local function validateResponse(pData, pts_Req)
-  local ts_res = timestamp()
-  local act_delay = ts_res - pts_Req
-  local actPayload = common.bson_to_table(pData.binaryData)
-  common.log("Delay:", act_delay)
-  common.log("SDL->MOB: App" ..pData.sessionId.." StartServiceAck(7) " .. common.tableToString(actPayload))
-  if act_delay < exp_delay - tolerance or act_delay > exp_delay + tolerance then
-    return false, "Expected delay: " .. exp_delay .. "ms, actual: " .. act_delay .. "ms"
+  local function validateResponse(pData, pts_Req)
+    local ts_res = timestamp()
+    local act_delay = ts_res - pts_Req
+    local actPayload = common.bson_to_table(pData.binaryData)
+    common.log("Delay:", act_delay)
+    common.log("SDL->MOB: App" ..pData.sessionId.." StartServiceAck(7) " .. common.tableToString(actPayload))
+    if ts_get_si == nil then
+      return false, "StartServiceAck received before receiving of GetSystemInfo from HMI"
+    end
+    if ts_get_vt == nil and pDelayGetVT ~= -1 then
+      return false, "StartServiceAck received before receiving of GetVehicleType from HMI"
+    end
+      return compareValues(rpcServiceAckParams, actPayload, "binaryData")
   end
-    return true
+
+  mobSession:ExpectControlMessage(common.serviceType.RPC, {
+    frameInfo = common.frameInfo.START_SERVICE_ACK,
+    encryption = false
+  })
+  :ValidIf(function(exp, data)
+    if exp.occurences == 1 and data.frameInfo == common.frameInfo.START_SERVICE_ACK then
+      return validateResponse(data, ts_req1)
+    elseif exp.occurences == 2 and data.frameInfo == common.frameInfo.START_SERVICE_ACK then
+      return validateResponse(data, ts_req2)
+    end
+      return false, "Unexpected message have been received"
+  end)
+    :Times(2)
 end
 
-mobSession:ExpectControlMessage(common.serviceType.RPC, {
-  frameInfo = common.frameInfo.START_SERVICE_ACK,
-  encryption = false
-})
-:ValidIf(function(exp, data)
-  if exp.occurences == 1 and data.frameInfo == common.frameInfo.START_SERVICE_ACK then
-    return validateResponse(data, ts_req1)
-  elseif exp.occurences == 2 and data.frameInfo == common.frameInfo.START_SERVICE_ACK then
-    common.getHMIConnection():RaiseEvent(pStartEvent, "Start event")
-    return validateResponse(data, ts_req2)
+local function start()
+  local function check()
+    delayedStartServiceAckMultipleApps(hmiCap, delay1, delay2)
   end
-    return false, "Unexpected message have been received"
-end)
-  :Times(2)
+  common.startWithExtension(check)
 end
 
 --[[ Scenario ]]
@@ -68,8 +104,7 @@ common.Title("Preconditions")
 common.Step("Clean environment", common.preconditions)
 
 common.Title("Test")
-common.Step("Start SDL, HMI, connect Mobile, start Session, send StartService", common.startWithExtension,
-  { startServiceMultipleApps })
+common.Step("Start SDL, HMI, connect Mobile, start Session, send StartService", start)
 
 common.Title("Postconditions")
 common.Step("Stop SDL", common.postconditions)
